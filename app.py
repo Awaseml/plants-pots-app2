38961e6e77133c8f3b07bb5a1c2fbc64b72a30ae
Awaseml <89212768+Awaseml@users.noreply.github.com>
2026-01-22 00:55:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
-import streamlit as st
 import psycopg2
-from fpdf import FPDF
+import streamlit as st
 
 def get_conn():
-    return psycopg2.connect(
-        st.secrets["DATABASE_URL"]
-    )
-
-conn = get_conn()
-
-st.title("Plants Pots App")
-
-st.success("Database connected successfully!")
+    return psycopg2.connect(st.secrets["DATABASE_URL"])
